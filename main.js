3b7fc7f78bba46805b589ed421307efa763962db
Monica Aparecida Maciel Montania <106695860+monicamontania@users.noreply.github.com>
2024-07-04 08:39:03 -0300

    04/07

---
+++ main.js
@@ -0,0 +1,14 @@
+const caixaPricipal = document.querySelector('.caixa-pricipal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativa');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultados = document.querySelector('.texto-resultados');
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
