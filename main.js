03c5881b4b9ed7e4c196b6da69de30f7aa577d25
Monica Aparecida Maciel Montania <106695860+monicamontania@users.noreply.github.com>
2024-07-24 08:46:26 -0300

    24/07

--- main.js
+++ main.js
@@ -5,10 +5,64 @@ const caixaResultados = document.querySelector('.caixa-resultados');
 const textoResultados = document.querySelector('.texto-resultados');
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciando: "Pergunta 1",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
-    }
+    },
+    {
+        enunciando:"Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    
+
+    },
+    {
+        enunciando:"Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+
+    },
+    {
+        enunciando:"Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciando:"Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
 ]
+
+  let atual = 0;
+  let perguntaAtual;
+
+  function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciando;
+  }
+
+  mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciando;
+    mostraAlternativas();
+  }
+
+  function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+  }
+  
