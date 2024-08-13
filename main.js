fcc9f76184455e3ec28ff70c86d5932dca9f1b75
Monica Aparecida Maciel Montania <106695860+monicamontania@users.noreply.github.com>
2024-08-13 09:16:45 -0300

    13/08

--- main.js
+++ main.js
@@ -1,120 +1,112 @@
-const caixaPricipal = document.querySelector('.caixa-pricipal');
+const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativa');
-const caixaResultados = document.querySelector('.caixa-resultados');
-const textoResultados = document.querySelector('.texto-resultados');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
 const perguntas = [
     {
-        enunciando: "Enunciado 01",
+        enunciado: "Enunciado 01",
         alternativas: [
             {
-            texto:"alternativa 01",
-            afirmacao:"resulatdo 01"
+                texto:"alternativa 01",
+                afirmacao: "resultado 01"
             },
             {
-            texto: "alternativa 2",
-            afirmacao: "resultado 2"
+                texto:"alternativa 02",
+                afirmacao: "resultado 02"
+            }   
+        ]
+    },
+    {
+        enunciado: "Enunciado 02",
+        alternativas: [
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
             }
         ]
     },
     {
-        enunciando:"Enunciado 02",
+        enunciado: "Enunciado 03",
         alternativas: [
             {
-                texto:"alternativa 03",
-                afirmacao:"resulatdo 03"
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
             },
             {
-                texto:"alternativa 04",
-                afirmacao:"resulatdo 04"
-            }
-            
-        
-        ]
-    
-    },
-    {
-        enunciando:"Enunciado 03",
-        alternativas: [
-            {
-                texto:"alternativa 04",
-                afirmacao:"resultado 04"
-            },
-            {
-                texto:"alternativa 05",
-                afirmacao:"resultado 05"
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
             }
         ]
-
     },
     {
-        enunciando:"Enunciado 04",
+        enunciado: "Enunciado 04",
         alternativas: [
-          {
-            texto:"alternativa 06",
-            afirmacao:"resultado 06"
-          },
-          {
-            texto:"alternativa 07",
-            afirmacao:"resultado 07"
-          }
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
         ]
     },
     {
-        enunciando:"Enunciado 05",
+        enunciado: "Enunciado 05",
         alternativas: [
             {
-                texto:"alternativa 08",
-                afirmacao:"resultado 08"
+                texto: "alternativa 09",
+                afirmacao: "resultado 09"
             },
             {
-                texto:"alternativa 09",
-                afirmacao:"resultado 09"
+                texto: "alternativa 10",
+                afirmacao: "resultado 10"
             }
         ]
     },
 ]
 
-  let atual = 0;
-  let perguntaAtual;
-  let historiaFinal = "";
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
-  function mostraPergunta(){
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciando;
-  }
-
- function mostraPerguntas(){
-    if(atual >= perguntaAtual.length){
-      mostraResultado();
-      return;
+function mostrarPerguntas(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return
     }
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciando;
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = ""
-    mostraAlternativas();
-  }
+    mostrarAlternativas();
+}
 
-  function mostraAlternativas(){
+function mostrarAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        caixaAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas);
     }
-  }
+}
 
-  function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " " 
+    historiaFinal += afirmacoes + " ";
     atual++;
-    mostraPergunta();
-  }
+    mostrarPerguntas();
+}
 
-  function mostraResultado(){
-    caixaPerguntas. textContent = "Inicio do texto..."
+function mostraResultado(){
+    caixaPerguntas.textContent = " Inicio do texto... "
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+}
 
-  }
-  
+mostrarPerguntas();
